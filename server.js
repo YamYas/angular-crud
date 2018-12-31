e604b1b936522e21b76d930311247cd49c0c77a1
DESKTOP-9DGD90B\Yamin <yyaminyasin@gmail.com>
2018-12-31 12:30:23 +0500

    Prepare for Heroku

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist2/angular-crud'));
+console.log(__dirname)
 
 // our apis
 
 app.all('*', (req, res)=>{
-    res.status(200).sendFile(__dirname + '/dist/angular-crud/index.html');
+    res.status(200).sendFile(__dirname + '/dist2/angular-crud/index.html');
 })
 
 app.listen(process.env.PORT || 8080);
